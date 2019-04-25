2a0efc03a39ce7cf2ecb5d3d767f746084e014ae
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-04-25 14:40:01 +0800

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -2,5 +2,25 @@
 
 
 
+if(isset($_POST['signup-btn'])){
+
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	$email = $_POST['email'];
+	$mfname = $_POST['mfname'];
+	$mlname = $_POST['mlname'];
+	$username = $_POST['username'];
+	$password1 = $_POST['password1'];
+	$password2 = $_POST['password2'];
+	$position = $_POST['position'];
+	
+
+if(empty($fname) || empty($lname) || empty($email) || empty($mfname) || empty($mlname) || empty($username) || empty($password1) || 
+	empty($password2)){
+	header("Location: ../signup.php?emptyfield");
+}
+}
+
+
 
 ?>
