3b2552d591e6dad38e1c6ac36424c66899034f08
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-04-25 10:18:14 +0800

    Update login.php

--- login.php
+++ login.php
@@ -33,32 +33,30 @@
 				<h3>Login</h3>
 			</div>
 			<div class="card-body">
-				<form>
+				<form action="/includes/signup.inc.php" method="POST">
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
 						</div>
-						<input type="text" class="form-control" placeholder="Email">
-						
+						<input type="text" class="form-control inputfield" placeholder="Username/Email" name="uid">
 					</div>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-key"></i></span>
 						</div>
-						<input type="password" class="form-control" placeholder="password">
+						<input type="password" class="form-control inputfield" placeholder="Password" name="password">
 					</div>
 					<div class="row align-items-center remember">
 						<input type="checkbox">Remember Me
 					</div>
+					<hr>
 					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
+						<input type="submit" value="Login" class="btn float-left login_btn" id="loginbtn" name="login">	
+						<button class="btn float-right login_btn" id="signupbtn" name="signup" href="signup.php"> Signup </button>
 					</div>
 				</form>
 			</div>
 			<div class="card-footer">
-				<div class="d-flex justify-content-center links">
-					Don't have an account?<a href="includes/signup.inc.php">Sign Up!</a>
-				</div>
 				<div class="d-flex justify-content-center links">
 					<a href="#">Forgot your password?</a>
 				</div>
