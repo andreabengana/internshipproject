7f701b6509abc1d93ff8ccd136d462c589e83f1f
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-04-26 11:40:24 +0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,19 +1,27 @@
 <?php
+session_start();;
 
+if (isset($_SESSION['userUid'])) {
+echo'
+<form action="includes/logout.inc.php" method="POST">
+	<button type="submit" class="btn btn-danger" name="logout-btn"> Logout </button>
+</form>';
+}
+else{
+	header("Location: login.php");	
 
-/* if(isset($_SESSION['userUid'])){
-	echo "tangina mo jer";
-} */
+}
+
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"</title>
+	<title> Andre </title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-	<h1>tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"</h1>
 
-	<a href="logout.php">Logout</a>
+
 </body>
 </html>
