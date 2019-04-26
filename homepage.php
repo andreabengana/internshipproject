8f3d7babe2360ae1682f80381fda4efd846360bf
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-04-26 14:17:48 +0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -17,11 +17,15 @@ else{
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Andre </title>
+	<!-- Meta Tags -->
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title> Home </title>
+	<link rel="stylesheet" type="text/css" href="css/homepagedesign.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
 
-
+	
 </body>
 </html>
