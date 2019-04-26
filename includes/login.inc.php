b99916f18da29408bd99319e64c370c215103450
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-04-26 08:54:04 +0800

    Update login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -13,6 +13,33 @@ if(empty($mailuid) || empty($password)){
 	exit();
 }
 
+else{
+	$sql = "SELECT * FROM users WHERE user_uid=? OR user_email=?;";
+	$stmt = mysqli_stmt_init($conn);
+	if(!mysqli_stmt_prepare($stmt, $sql)){
+		header("Location: ../login.php?sqlerror");	
+		exit();
+	}
+	else{
+		mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
+		mysqli_stmt_execute($stmt);
+		$result = mysqli_stmt_get_result($stmt);
+		if($row = mysqli_fetch_assoc($result)){
+			if(!$row['user_pwd'] === $password){
+				header("Location: ../login.php?invalidpassword");
+			}
+			else{
+				session_start();
+				$_SESSION['userUid'] = $row['user_uid'];
+
+				header("Location: ../homepage.php");
+			}
+		}
+		else{
+			header("Location:  ../login.php?nouser");
+		}
+	}
+}
 
 }
 else{
