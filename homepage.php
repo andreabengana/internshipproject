1911c87adcd393432b8ccf8bb7fbdd48e99856ae
Andre Abengana <john.abengana@outlook.com>
2019-04-26 10:54:06 +0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,17 +1,19 @@
 <?php
 
 
-if(isset($_SESSION['userUid'])){
+/* if(isset($_SESSION['userUid'])){
 	echo "tangina mo jer";
-}
+} */
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"</title>
 </head>
 <body>
-	
+	<h1>tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"tangina mo jer"</h1>
+
+	<a href="logout.php">Logout</a>
 </body>
 </html>
