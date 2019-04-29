8c6900d4eb2734107b7240f39709f58504d21411
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-04-29 17:02:02 +0800

    Update navbar.inc.php

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -1,28 +1,34 @@
+<?php
 
+if(!isset($_SESSION['userUid'])){
+	header("Location: ../login.php");
+}
+
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> Name of Project </title>   		
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">	
 </head>
 <body>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Login Page</title>
+	
    <!--Made with love by Gerald Anderson -->
    
-	<!--Bootsrap 4 CDN-->
+	<!--Bootsrap 4 	-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    
-    <!--Fontawesome CDN-->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="https://andersonbpoinc.com/">
+  	<img src="resources/agbilogo.png" style="height:50px; width: 50px;">
+  </a>
+ <form class="form-inline" action="includes/logout.inc.php" method="POST">
+  <button type="submit" id="logout-btn" name="logout-btn" class="btn btn-outline-danger">Logout</button>
+</form>
+</nav>
 
-
-
-
+</body>
+</html>
