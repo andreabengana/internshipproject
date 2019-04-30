78e353aa5c9f90cf79cce212163603b706553985
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-04-30 14:17:54 +0800

    Update navbar.inc.php

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -11,16 +11,14 @@ if(!isset($_SESSION['userUid'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-
-</head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../internshipproject/css/navbardesign.css">
 <body>
 	
    <!--Made with love by Gerald Anderson -->
    
 	<!--Bootsrap 4 	-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<nav class="navbar navbar-expand-lg" style="background-color: skyblue;"  id="navbar">
+<nav class="navbar navbar-expand-lg" id="navbar">
   <a class="navbar-brand" href="https://andersonbpoinc.com/">
   	<img src="resources/agbilogo.png" style="height:50px; width: 50px;">
   </a>
