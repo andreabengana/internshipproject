162899e0c2106b00e94a98afa4fc3ce059c2804c
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-04-30 13:45:06 +0800

    Update navbar.inc.php

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -4,16 +4,15 @@ if(!isset($_SESSION['userUid'])){
 	header("Location: ../login.php");
 }
 
-
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">	
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
 </head>
 <body>
 	
@@ -21,11 +20,12 @@ if(!isset($_SESSION['userUid'])){
    
 	<!--Bootsrap 4 	-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<nav class="navbar navbar-expand-lg" style="background-color: skyblue;">
+<nav class="navbar navbar-expand-lg" style="background-color: skyblue;"  id="navbar">
   <a class="navbar-brand" href="https://andersonbpoinc.com/">
   	<img src="resources/agbilogo.png" style="height:50px; width: 50px;">
   </a>
 </nav>
+
 
 
 </body>
