999b723b7fe4b810e5d213d0521d170648327288
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-04-30 10:43:20 +0800

    Update navbar.inc.php

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -21,13 +21,10 @@ if(!isset($_SESSION['userUid'])){
    
 	<!--Bootsrap 4 	-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg" style="background-color: skyblue;">
   <a class="navbar-brand" href="https://andersonbpoinc.com/">
   	<img src="resources/agbilogo.png" style="height:50px; width: 50px;">
   </a>
- <form class="form-inline" action="includes/logout.inc.php" method="POST">
-  <button type="submit" id="logout-btn" name="logout-btn" class="btn btn-outline-danger">Logout</button>
-</form>
 </nav>
 
 
