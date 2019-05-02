9a89e17d16665c6185148df36bc80fe636a374fa
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-05-02 15:11:39 +0800

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,6 +1,6 @@
 <?php 
 
-
+include 'ChromePHP.php';
 
 if(isset($_POST['signup-btn'])){
 	require 'db.inc.php';
@@ -35,11 +35,11 @@ elseif(!preg_match("/^[a-zA-Z0-9]*$/", $username)){
 	header("Location: ../addemployee.php?invalidusername");
 	exit();
 }
-elseif($password !== $password2){
+elseif($password1 !== $password2){
 	header("Location: ../addemployee.php?passwordsdontmatch");
 }
 else{
-	$sql = "SELECT user_uid FROM users WHERE user_uid=?";
+	$sql = "SELECT * FROM users WHERE user_uid=? OR user_email=?";
 	$stmt = mysqli_stmt_init($conn);
 
 	if(!mysqli_stmt_prepare($stmt, $sql)){
@@ -47,19 +47,37 @@ else{
 	exit();
 	}
 	else{
-		mysqli_stmt_bind_param($stmt, "s", $username);
+		mysqli_stmt_bind_param($stmt, "ss", $username, $email);
 		mysqli_stmt_execute($stmt);
 		mysqli_stmt_store_result($stmt);
 		$result = mysqli_stmt_num_rows($stmt);
 		if($result > 0){
-			header("Location: ../addemployee.php?usernametaken")
+			header("Location: ../addemployee.php?usernameoremailtaken");
 			exit();
 		}
 		else{
-			$sql = "INSERT INTO users "
+			$sql = "INSERT INTO users (user_uid, user_email, user_password, user_type, user_fname, user_lname, manager_fname, manager_lname) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+			$stmt = mysqli_stmt_init($conn);
+
+			if(!mysqli_stmt_prepare($stmt, $sql)){
+				header("Location: ../addemployee.php?error=sqlerror");
+				exit();
+			}
+			else{
+				$hashedpwd = password_hash($password, PASSWORD_DEFAULT);
+				mysqli_stmt_bind_param($stmt, "ssssssss", $username, $email, $hashedpwd, $position, $fname, $lname, $mfname, $mlname);
+				mysqli_stmt_execute($stmt);
+				header("Location: ../addemployee.php?employeeadded");
+				exit();
+			}
 		}	
 	}	
 }
+mysqli_stmt_close($stmt);
+mysqli_close($conn);
+}
+else{
+	header("Location: login.php");
 }
 
 
