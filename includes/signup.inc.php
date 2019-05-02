47a7e158f2283501bc5b7eb4aad0618d16636b8a
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-05-02 10:24:45 +0800

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -3,6 +3,8 @@
 
 
 if(isset($_POST['signup-btn'])){
+	include 'db.inc.php';
+
 
 	$fname = $_POST['fname'];
 	$lname = $_POST['lname'];
@@ -18,9 +20,27 @@ if(isset($_POST['signup-btn'])){
 if(empty($fname) || empty($lname) || empty($email) || empty($mfname) || empty($mlname) || empty($username) || empty($password1) || 
 	empty($password2)){
 	header("Location: ../addemployee.php?emptyfield");
+	exit();
+}
+elseif(!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)){
+	header("Location: ../addemployee.php?invalide-mail&username");
+	exit();
+
+}
+elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+	header("Location: ../addemployee.php?invalide-mail");
+	exit();
+}
+elseif(!preg_match("/^[a-zA-Z0-9]*$/", $username)){
+	header("Location: ../addemployee.php?invalidusername");
+	exit();
+}
+elseif($password !== $password2){
+	header("Location: ../addemployee.php?passwordsdontmatch");
 }
 else{
-
+	$sql = "SELECT user_uid FROM users WHERE user_uid=?";
+	$stmt = mysqli_stmt_prepare($conn);
 }
 }
 
