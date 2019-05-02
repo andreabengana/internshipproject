174834e7a85ed9578a94e054e8ab6c7dcc062107
Andre Abengana <john.abengana@outlook.com>
2019-05-02 09:08:52 +0800

    moved the navbar left by 257px, removed search bar and notif buttons

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -51,13 +51,13 @@ if(!isset($_SESSION['userUid'])){
                 <div class="section__content section__content--p30">
                     <div class="container-fluid">
                         <div class="header-wrap">
-                            <form class="form-header" action="" method="POST">
+                            <!-- <form class="form-header" action="" method="POST">
                                 <input class="au-input au-input--xl" type="text" name="search" placeholder="Search for datas &amp; reports..." />
                                 <button class="au-btn--submit" type="submit">
                                     <i class="zmdi zmdi-search"></i>
                                 </button>
-                            </form>
-                            <div class="header-button">
+                            </form> -->
+                            <!-- <div class="header-button">
                                 <div class="noti-wrap">
                                     <div class="noti__item js-item-menu">
                                         <i class="zmdi zmdi-comment-more"></i>
@@ -213,7 +213,7 @@ if(!isset($_SESSION['userUid'])){
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
                         </div>
                     </div>
                 </div>
