26f27b49ca42a3c423423d03af605e374ebcad4d
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-05-03 09:31:39 +0800

    Update addmanager.php

--- addmanager.php
+++ addmanager.php
@@ -25,17 +25,17 @@ if(!isset($_SESSION['userUid'])){
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     
 	<!--Custom CSS-->
-	<link rel="stylesheet" type="text/css" href="css/signupdesign.css">
+	<link rel="stylesheet" type="text/css" href="css/addmanagerdesign.css">
 </head>
 <body>
 <div class="container">
 	<div class="d-flex justify-content-center h-100">
 		<div class="card">
 			<div class="card-header text-center">
-				<a href="homepage.php" class="btn btn-danger float-left">Go Back</a><h2 style="color: white;">Add Employee</h2>
+				<a href="homepage.php" class="btn btn-danger float-left">Go Back</a><h2 style="color: white;">Add Manager</h2>
 			</div>
 			<div class="card-body">
-						<form id="form1" action="includes/signup.inc.php" method="POST">
+						<form id="form1" action="includes/addmanager.inc.php" method="POST">
 							<label for="row1"><h3> Tell me more about yourself! </h3></label>
   							<div class="form-row" id="row1">
     							<div class="form-group col-md-6">
@@ -51,18 +51,7 @@ if(!isset($_SESSION['userUid'])){
   									<label for="inputemail"> Email </label>
   									<input type="text" name="email" class="form-control" placeholder="someone@example.com">
   							</div>
-  							<label for="row2"><h3>Who's your Manager?</h3></label>
-  							<div class="form-row" id="row2">
-  								<div class="form-group col-md-6">
-      								<label for="inputmfirstname">Manager's First Name</label>
-      								<input type="text" class="form-control" id="inputmfirstname" placeholder="John Mark" name="mfname">
-    							</div>
-    							<div class="form-group col-md-6">
-      								<label for="inputmlname">Last Name</label>
-      								<input type="text" class="form-control" id="inputmlname" placeholder="Causing" name="mlname">
-    							</div>
-  							</div>
-  							<label for="row3"><h3>Account Details</h3></label>
+      							<label for="row3"><h3>Account Details</h3></label>
   							<div class="form-row" id="row3">
 									<div class="form-group col-md-12">
   										<label for="username"> Username </label>
@@ -78,11 +67,11 @@ if(!isset($_SESSION['userUid'])){
 									</div>
   							</div>
   							<label for="position">Sign Up as</label>
-  							<select class="form-control" name="position" value="Staff">
-                <option>Staff</option>
+  							<select class="form-control" name="position" value="Manager">
+                <option>Manager</option>
                 </select> 								
                 <br>
- 								<button type="submit" class="btn btn-lg btn-success btn-block" name="signup-btn">Add Employee</button>
+ 								<button type="submit" class="btn btn-lg btn-success btn-block" name="signup-btn">Add Manager</button>
 						</form>
 			</div>
 		</div>
